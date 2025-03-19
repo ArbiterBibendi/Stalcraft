ef2cec903c130b687290ad91dc7a54b9aedf4df2
Drake Scott <ArbiterBibendi@yahoo.com>
2025-03-19 15:21:20 -0500

    itemdb stuff

--- itemdb.js
+++ itemdb.js
@@ -22,7 +22,34 @@ const getItemIdByName = (name) => {
 const getEnglishName = (item) => {
   return item.name.type === "translation" ? item.name.lines.en : item.name.text;
 };
+const getItemId = (item) => {
+  return item.data.split("/")[4].split(".")[0];
+};
+const ItemCategories = {
+  Armor: "armor",
+  Artefact: "artefact",
+  Attachment: "attachment",
+  Bullet: "bullet",
+  Containers: "containers",
+  Drink: "drink",
+  Food: "food",
+  Grenade: "grenade",
+  Medicine: "medicine",
+  Misc: "misc",
+  Other: "other",
+  Weapon: "weapon",
+};
+const getItemsOfType = (category) => {
+  const listings = itemListings.filter((listing) => {
+    const listingCategory = listing.data.split("/")[2];
+    return listingCategory == category;
+  });
+  return listings;
+};
 
 module.exports.findItemByName = findItemByName;
 module.exports.getItemIdByName = getItemIdByName;
 module.exports.getEnglishName = getEnglishName;
+module.exports.getItemsOfType = getItemsOfType;
+module.exports.ItemCategories = ItemCategories;
+module.exports.getItemId = getItemId;
